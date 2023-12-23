c89c993a878d35bbe57d51b46ba297d58d5792ad
Kavin Abregana <abreganakavin@gmail.com>
2023-12-23 22:13:41 +0800

    change to create-react-app from vite

---
+++ src/pages/Homepage.js
@@ -0,0 +1,17 @@
+import Header from "../components/Header";
+import Banner from "../components/Banner";
+import Main from "../components/Main";
+import Footer from "../components/Footer";
+
+function Homepage() {
+  return (
+    <>
+      <Header />
+      <Banner />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default Homepage;
