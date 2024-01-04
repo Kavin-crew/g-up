af5cd7b266d3a0fe54ea67c174a4ce7aafe394ae
Kevin Webster Revalde <kevinwebsterr2016@gmail.com>
2024-01-04 23:38:10 +0800

    banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,7 +1,22 @@
 function Banner() {
     return (
         <section className="banner">
-            <h1 className="heading--primary">Profile</h1>
+            <div className="wrapper">
+                <div className="banner_con">
+                    <div className="banner_con_info">
+                        <h1 className="heading heading-primary">
+                            <small>Uncover Scammers:</small>
+                            Search, Verify,
+                            <span>Protect!</span>
+                        </h1>
+                        <p>Explore Our Tool to Identify Scam Phone Numbers Instantly.</p>
+                    </div>
+
+                    <figure>
+                        <img src="images/image.png" alt="woman smiling" />
+                    </figure>
+                </div>
+            </div>
         </section>
     );
 }
