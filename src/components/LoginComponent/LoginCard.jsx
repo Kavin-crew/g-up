44669c557d0512ae89191f3cf55bf7c624c621b6
Kevin Webster Revalde <kevinwebsterr2016@gmail.com>
2024-02-06 01:14:58 +0800

    login authentication

--- src/components/LoginComponent/LoginCard.jsx
+++ src/components/LoginComponent/LoginCard.jsx
@@ -1,146 +1,196 @@
-import { useEffect, useRef, useState } from "react";
-import { IoMdEye ,IoMdEyeOff  } from "react-icons/io";
-import { MdOutlinePhoneAndroid } from "react-icons/md";
-const LoginCard = ({setIsOpen , setIsLogin}) => {
+import { useEffect, useRef, useState, useContext } from 'react';
+import { IoMdEye, IoMdEyeOff } from 'react-icons/io';
+import { MdOutlinePhoneAndroid } from 'react-icons/md';
+import AuthContext from '../../context/AuthProvider';
+import { toast } from 'react-toastify';
 
-const [TogglePass, setTogglePass] = useState(false)
-const [UserPhone, setUserPhone] = useState('')
-const [UserPass, setUserPass] = useState('')
-const userRef = useRef()
+const LoginCard = ({ setIsOpen, setIsLogin }) => {
+    const { setAuth } = useContext(AuthContext);
 
+    const [TogglePass, setTogglePass] = useState(false);
+    const [UserPhone, setUserPhone] = useState('');
+    const [UserPass, setUserPass] = useState('');
+    const userRef = useRef();
 
+    const toggleShow = () => {
+        setTogglePass(!TogglePass);
+    };
 
+    useEffect(() => {
+        userRef.current.focus();
+    }, []);
 
-const toggleShow = ()=>{
-    setTogglePass(!TogglePass)
-}
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.warn(`Phone: ${UserPhone}`);
+        console.warn(`Password: ${UserPass}`);
 
+        try {
+            const res = await fetch(`https://g-up-api.up.railway.app/api/v1/users/login`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    phoneNumber: UserPhone,
+                    password: UserPass,
+                }),
+                withCredentials: true,
+            });
+            console.log(res.ok);
+            const data = await res.json();
 
+            if (res.ok) {
+                const accessToken = data?.token;
 
+                const userDetails = data?.data;
+                setAuth({ userDetails, password, accessToken });
+                console.log(accessToken);
+                console.log(userDetails);
 
+                toast.success('Login Successfully!', {
+                    position: 'top-right',
+                    autoClose: 2000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: 'light',
+                });
+                setTimeout(function () {
+                    setUserPhone('');
+                    setUserPass('');
+                    setIsOpen(false);
+                }, 200);
+            } else {
+                toast.error(data.message, {
+                    position: 'top-right',
+                    autoClose: 3000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: 'light',
+                });
 
-useEffect(()=>{
-    userRef.current.focus()
-},[])
+                userRef.current.focus();
+            }
+        } catch (error) {
+            toast.error('No Server Response', {
+                position: 'top-right',
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                draggable: true,
+                progress: undefined,
+                theme: 'light',
+            });
+        }
+    };
 
+    const isClose = () => {
+        setIsOpen(false);
+    };
 
-
-
-
-
-const handleSubmit = async (e)=>{
-    e.preventDefault();
-    console.warn(`Phone: ${UserPhone}`)
-    console.warn(`Password: ${UserPass}`)
-
-
-    try {
-        const res = await fetch("https://g-up-api.up.railway.app/api/v1/users/login",{
-          method:"POST",
-          headers:{ 
-            "Content-Type":"application/json",
-          },
-          body: JSON.stringify({
-            "email": UserPhone,
-            "password": UserPass
-          }),
-        });
-
-        const data = await res.json()
-        console.log(data)
-
-
-      } catch (error) {
-        console.log(error)
-      }
-
-
-
-
-
-}
-
-const isClose =()=>{
-    setIsOpen(false)
-}
-
-const gotoRegister =()=>{
-    setIsLogin(false)
-
-}
-
-
+    const gotoRegister = () => {
+        setIsLogin(false);
+    };
 
     return (
         <div className="modal__card modal__card-login">
-        <div className="modal__card__cont modal__card__cont-login">
-        <span className="modal__card__cont__close" onClick={isClose}>X</span>
+            <div className="modal__card__cont modal__card__cont-login">
+                <span className="modal__card__cont__close" onClick={isClose}>
+                    X
+                </span>
 
-                 <h2>Welcome back!</h2>
+                <h2>Welcome back!</h2>
                 <p>Please enter your details</p>
                 <form className="modal__form" onSubmit={handleSubmit}>
-
                     <div className="modal__form__input">
-                    <figure className="modal__form__input__icon"> <MdOutlinePhoneAndroid size='2.1rem' color="#9D9D9D" /></figure>
-                        <input ref={userRef} type="text" placeholder="Phone Number" value={UserPhone} onChange={e=>setUserPhone(e.target.value)}
-                        required
+                        <figure className="modal__form__input__icon">
+                            {' '}
+                            <MdOutlinePhoneAndroid size="2.1rem" color="#9D9D9D" />
+                        </figure>
+                        <input
+                            ref={userRef}
+                            type="text"
+                            placeholder="Phone Number"
+                            value={UserPhone}
+                            onChange={(e) => setUserPhone(e.target.value)}
+                            required
                         />
                         <div className="line"></div>
                     </div>
 
                     <div className="modal__form__input">
-                    <figure className="modal__form__input__icon"> <img src='images/icons/form-key-icon.png' alt="Key Icon" /></figure>
-                        <input type={!TogglePass ? 'password' : 'text'} value={UserPass} onChange={e=>setUserPass(e.target.value)} placeholder="Password"/>
+                        <figure className="modal__form__input__icon">
+                            {' '}
+                            <img src="images/icons/form-key-icon.png" alt="Key Icon" />
+                        </figure>
+                        <input
+                            type={!TogglePass ? 'password' : 'text'}
+                            value={UserPass}
+                            onChange={(e) => setUserPass(e.target.value)}
+                            placeholder="Password"
+                        />
                         <div className="line"></div>
-                    <figure className="modal__form__input__icon pointer" onClick={toggleShow}> {!TogglePass ? <IoMdEye size='2.5rem' color="#C5C5C5" /> : <IoMdEyeOff size='2.5rem' color="#C5C5C5" />} </figure>
+                        <figure className="modal__form__input__icon pointer" onClick={toggleShow}>
+                            {' '}
+                            {!TogglePass ? <IoMdEye size="2.5rem" color="#C5C5C5" /> : <IoMdEyeOff size="2.5rem" color="#C5C5C5" />}{' '}
+                        </figure>
                     </div>
 
                     <div className="modal__form__info">
-
                         <div className="modal__form__info__remeberme">
                             <input type="checkbox" name="" id="rememberme" />
                             <label htmlFor="rememberme">Remember me</label>
                         </div>
 
                         <div className="modal__form__info__forgotpass">
-                            <a href="#" className="a-btn">Forgot Password?</a>
+                            <a href="#" className="a-btn">
+                                Forgot Password?
+                            </a>
                         </div>
                     </div>
 
-                    <button className="modal__form__btn btn-primary">
-                    Login
-                    </button>
-                    <p className="u-margin-top-xs">Don’t have an account? <a className="a-btn" onClick={()=>gotoRegister()}>Create one!</a></p>
+                    <button className="modal__form__btn btn-primary">Login</button>
+                    <p className="u-margin-top-xs">
+                        Don’t have an account?{' '}
+                        <a className="a-btn" onClick={() => gotoRegister()}>
+                            Create one!
+                        </a>
+                    </p>
                 </form>
 
                 <div className="thirdparty__card">
-                <section className="thirdparty__card__or">
-                   or
-                </section>
+                    <section className="thirdparty__card__or">or</section>
 
-            <div className="thirdparty__card__btncon">
-                <button className="thirdparty__btn thirdparty__btn--google">
-                <figure className="thirdparty__btn__icon"><img src='images/icons/form-gmail-icon.png' alt="Email Icon" /></figure>
-                    Sign In With Google
-                </button>
-                <button className="thirdparty__btn thirdparty__btn--facebook">
-                <figure className="thirdparty__btn__icon"><img src='images/icons/form-fb-icon.png' alt="Email Icon" /></figure>
-                Sign In With Facebook
-                </button>
+                    <div className="thirdparty__card__btncon">
+                        <button className="thirdparty__btn thirdparty__btn--google">
+                            <figure className="thirdparty__btn__icon">
+                                <img src="images/icons/form-gmail-icon.png" alt="Email Icon" />
+                            </figure>
+                            Sign In With Google
+                        </button>
+                        <button className="thirdparty__btn thirdparty__btn--facebook">
+                            <figure className="thirdparty__btn__icon">
+                                <img src="images/icons/form-fb-icon.png" alt="Email Icon" />
+                            </figure>
+                            Sign In With Facebook
+                        </button>
+                    </div>
+                </div>
             </div>
 
+            <div className="modal__card__bg modal__card__bg-login">
+                <h2 className="logo">g.up</h2>
+                <h3>Additional business details will be uploaded.</h3>
+                <figure className="modal__card__bg__img">
+                    <img src="images/login-img.png" alt="" />
+                </figure>
             </div>
-
-                </div>
-
-                <div className="modal__card__bg modal__card__bg-login">
-                    <h2 className="logo">g.up</h2>
-                    <h3>Additional business details will be uploaded.</h3>
-                    <figure className="modal__card__bg__img"><img src="images/login-img.png" alt="" /></figure>
-                </div>
-                
         </div>
     );
-}
+};
 
 export default LoginCard;
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { AuthProvider } from './context/AuthProvider.jsx';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <LocalizationProvider dateAdapter={AdapterDateFns}>
         <React.StrictMode>
-            <App />
+            <AuthProvider>
+               <App />
+            </AuthProvider>
         </React.StrictMode>
     </LocalizationProvider>
 );
